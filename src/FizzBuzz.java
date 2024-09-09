e741701895b0cfee2c11f67a75a02b0781c48e4a
Vennise Ho <venniseho14@gmail.com>
2024-09-09 14:15:49 -0400

    created Main file that runs FizzBuzz

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,7 +3,7 @@
  */
 class FizzBuzz {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         int i = 1;
         while (i < 100) {
@@ -12,7 +12,7 @@ class FizzBuzz {
         }
     }
 
-    private static void doFizzBuzz(int i) {
+    public static void doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
